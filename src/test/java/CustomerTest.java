8f94d1200f6a4eca9f6caaddeaea82f4539b786a
Ansgar <fehnker@gmail.com>
2019-05-25 10:08:24 +0200

    single test

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -14,7 +14,7 @@ public class CustomerTest {
         Pete.addRental(new Rental(Black,1));
         Pete.update();
 
-        assertEquals(Pete.getOwed(), 7.5);
+        assertEquals(Pete.getOwed(), 8.5);
         assertEquals(Pete.getPoints(), 3);
     }
 }

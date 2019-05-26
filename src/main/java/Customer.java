59c21a4677170ab1dfa39320f1b55798387e124f
Ansgar <fehnker@gmail.com>
2019-05-26 08:23:45 +0200

    Version 0.2 of the demo. Some reordering for the sake of the demo.

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -9,6 +9,18 @@ class Customer {
     private String _name;
     private Vector<Rental> _rentals = new Vector<Rental>();
 
+    public Customer(String name) {
+        _name = name;
+    }
+
+    public void addRental(Rental arg) {
+        _rentals.addElement(arg);
+    }
+
+    public String getName() {
+        return _name;
+    }
+
     public double getOwed() {
         double totalAmount = 0;
         Enumeration<Rental> rentals = _rentals.elements();
@@ -58,17 +70,7 @@ class Customer {
         return frequentRenterPoints;
     }
 
-    public Customer(String name) {
-        _name = name;
-    }
 
-    public void addRental(Rental arg) {
-        _rentals.addElement(arg);
-    }
-
-    public String getName() {
-        return _name;
-    }
 
 
 }

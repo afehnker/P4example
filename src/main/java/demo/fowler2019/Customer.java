c931db48db78abafb73423e8bb23a46d02aa35f8
Ansgar <fehnker@gmail.com>
2019-05-27 11:37:41 +0200

    Version 10.0. Refactored a lot.

--- src/main/java/demo/fowler2019/Customer.java
+++ src/main/java/demo/fowler2019/Customer.java
@@ -3,9 +3,6 @@ package demo.fowler2019;
 import java.util.Enumeration;
 import java.util.Vector;
 
-import java.util.Enumeration;
-import java.util.Vector;
-
 public class Customer {
     private String _name;
     private Vector<Rental> _rentals = new Vector<Rental>();
@@ -27,27 +24,9 @@ public class Customer {
         Enumeration<Rental> rentals = _rentals.elements();
 
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
             // determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
-            // show figures for this rental
-            totalAmount += thisAmount;
+            totalAmount += each.getCharge();
         }
         return totalAmount;
     }
@@ -59,14 +38,8 @@ public class Customer {
         while (rentals.hasMoreElements()) {
 
             Rental each = (Rental) rentals.nextElement();
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
-            // add frequent renter points
-            frequentRenterPoints++;
-
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
-            // show figures for this rental
         }
         return frequentRenterPoints;
     }
--- src/main/java/demo/fowler2019/Rental.java
+++ src/main/java/demo/fowler2019/Rental.java
@@ -16,4 +16,13 @@ public class Rental {
     public Movie getMovie() {
         return _movie;
     }
+
+    double getCharge() {
+        return _movie.getCharge(_daysRented);
+    }
+
+    int getFrequentRenterPoints() {
+
+        return _movie.getPoints(_daysRented);
+    }
 }
